4059ea0623574a0b8b3c482f1636e78764c0eb33
Agnieszka Prościak <120598348+agaprosciak@users.noreply.github.com>
2025-06-14 19:40:14 +0200

    Update Heatmapa.R

--- Heatmapa.R
+++ Heatmapa.R
@@ -27,12 +27,10 @@ generate_heatmap <- function(filepath, title) {
 # Wygeneruj heatmapy dla 3 zestawów danych
 heatmap1 <- generate_heatmap("HistoricData_with_CAQI.csv", "Korelacje – Dane historyczne")
 heatmap2 <- generate_heatmap("24HData_with_CAQI.csv", "Korelacje – Ostatnie 24h")
-heatmap3 <- generate_heatmap("TodaysData_with_CAQI.csv", "Korelacje – Dzisiaj")
 heatmap4 <-generate_heatmap("2023_data_with_CAQI.csv","Korelacje - rok 2023")
 heatmap5<-generate_heatmap("merged_energy_pollution_weather.csv","Korelacje - jakość powietrza a pogoda")
 
 print(heatmap1)
 print(heatmap2)
-print(heatmap3)
 print(heatmap4)
 print(heatmap5)
